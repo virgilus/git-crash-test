49c75a0e7fcdb6c4b1b2a7c1ed4d336e47c10937
Adubea <gloriaadubeaawuku@gmail.com>
2026-09-21 13:27:26 +0200

    Thanks

--- preprocess.py
+++ preprocess.py
@@ -2,14 +2,18 @@ import pandas as pd
 
 df = pd.read_csv("dataset.csv")
 print(df.describe())
-<<<<<<< HEAD
+
+#Added by Adubea
+print("Adubea Average score:", df["score"].mean())
+
 # Virgile
 print(df['age'].mean())
-=======
+
 #Added by Vicente
 print("Average score:", df["score"].mean())
->>>>>>> main
+
 =======
+
 # Added by Rodrigo
 print("Average score:", df["score"].mean())
 
