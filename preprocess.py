4e7b4bba1a50f42475f4aa76e19bad03f37696cb
Adubea <gloriaadubeaawuku@gmail.com>
2026-09-21 13:39:54 +0200

    Update preprocessing

--- preprocess.py
+++ preprocess.py
@@ -3,19 +3,17 @@ import pandas as pd
 df = pd.read_csv("dataset.csv")
 print(df.describe())
 
-#Added by Adubea
-print("Adubea Average score:", df["score"].mean())
-
 # Virgile
 print(df['age'].mean())
 
 #Added by Vicente
 print("Average score:", df["score"].mean())
 
-=======
-
 # Added by Rodrigo
 print("Average score:", df["score"].mean())
 
 #Added by Ebube
 print("Average score:", df["score"].mean())
+
+#Added by Adubea
+print("Adubea Average score:", df["score"].mean())
